4993fafe127173f1094c4bf5edf42167a1b4f4d1
Camila Rosat Consiglio <consigliocr@gmail.com>
2020-09-17 15:12:02 +0200

    Update Figure7.R

--- Figure7.R
+++ Figure7.R
@@ -1,11 +1,16 @@
 # The Immunology of Multisystem Inflammatory Syndrome in Children with COVID-19
 # Figure 7
 
+# Download data from Mendeley Data in Figure_7 folder.
+# http://dx.doi.org/10.17632/ds6g796xyg.1
+
+
 # Preprocessing
 # Sample IDs
-IDs <- read.csv("https://ki.box.com/shared/static/uq3bns63ebakzqbtzuduqivcyalwiv2k.csv", header = T)
+IDs <- read.csv("Figure7-ID.csv", header = T)
+
 # Quantile-normalized autoAb target dataset
-autoAbs <- read.csv("https://ki.box.com/shared/static/z4a4torxyevlx5a00ksp833bi87lofkc.csv", header = T, stringsAsFactors = FALSE)
+autoAbs <- read.csv("MIS_C.Experiment.QuantileNormailized.200712.csv", header = T, stringsAsFactors = FALSE)
 library(stringr)
 autoAbs$uORF <- sapply(str_split(autoAbs$Name, "~"), "[", 3)
 autoAbs$uORF <- str_remove(autoAbs$uORF, "uORF:")
@@ -40,7 +45,7 @@ mylevelsS1 <- c("2917","2865","3073","3080","3079","3236","2984","2963","2973","
                 "Kawasaki_4",  "Kawasaki_8", "Kawasaki_1", "Kawasaki_6", "Kawasaki_11", "Kawasaki_5", "Kawasaki_9")
 m_autoAbs$Subjects <- factor(m_autoAbs$Subjects, levels = mylevelsS1)
 
-# Figure 6A
+# Figure 7A
 library(ggplot2)
 ggplot(m_autoAbs, aes(y=log1p(value), x=Subjects)) + 
   geom_violin(aes(fill=Group, colour=Group)) +
@@ -100,15 +105,15 @@ Healthy_MISC_2 <- Healthy_MISC_1[,c("ensembl_ID", "rank")]
 Healthy_MISC_2 <- deframe(Healthy_MISC_2)
 set.seed(1234)
 GGO_HvsM <- gseGO(Healthy_MISC_2, 
-                ont="BP",
-                keyType = 'ENSEMBL',
-                nPerm=1000,
-                minGSSize = 60, 
-                maxGSSize = 500, 
-                pvalueCutoff = 0.05,
-                verbose = T,
-                OrgDb='org.Hs.eg.db',
-                seed = TRUE) 
+                  ont="BP",
+                  keyType = 'ENSEMBL',
+                  nPerm=1000,
+                  minGSSize = 60, 
+                  maxGSSize = 500, 
+                  pvalueCutoff = 0.05,
+                  verbose = T,
+                  OrgDb='org.Hs.eg.db',
+                  seed = TRUE) 
 GGO_HvsM <- clusterProfiler::simplify(GGO_HvsM)
 GGO_HvsM_df <- GGO_HvsM[,1:11]
 GGO_HvsM_df <- GGO_HvsM_df[order(GGO_HvsM_df$NES, decreasing = TRUE),]
@@ -122,15 +127,15 @@ CoV_MISC_2 <- CoV_MISC_1[,c("ensembl_ID", "rank")]
 CoV_MISC_2 <- deframe(CoV_MISC_2)
 set.seed(1234)
 GGO_CvsM <- gseGO(CoV_MISC_2, 
-                ont="BP",
-                keyType = 'ENSEMBL',
-                nPerm=1000,
-                minGSSize = 60, 
-                maxGSSize = 500, 
-                pvalueCutoff = 0.05,
-                verbose = T,
-                OrgDb='org.Hs.eg.db',
-                seed = TRUE) 
+                  ont="BP",
+                  keyType = 'ENSEMBL',
+                  nPerm=1000,
+                  minGSSize = 60, 
+                  maxGSSize = 500, 
+                  pvalueCutoff = 0.05,
+                  verbose = T,
+                  OrgDb='org.Hs.eg.db',
+                  seed = TRUE) 
 GGO_CvsM <- clusterProfiler::simplify(GGO_CvsM)
 GGO_CvsM_df <- GGO_CvsM[,1:11]
 GGO_CvsM_df <- GGO_CvsM_df[order(GGO_CvsM_df$NES, decreasing = TRUE),]
@@ -144,15 +149,15 @@ Kawasaki_MISC_2 <- Kawasaki_MISC_1[,c("ensembl_ID", "rank")]
 Kawasaki_MISC_2 <- deframe(Kawasaki_MISC_2)
 set.seed(1234)
 GGO_KvsM <- gseGO(Kawasaki_MISC_2, 
-                ont="BP",
-                keyType = 'ENSEMBL',
-                nPerm=1000,
-                minGSSize = 60, 
-                maxGSSize = 500, 
-                pvalueCutoff = 0.05,
-                verbose = T,
-                OrgDb='org.Hs.eg.db',
-                seed = TRUE) 
+                  ont="BP",
+                  keyType = 'ENSEMBL',
+                  nPerm=1000,
+                  minGSSize = 60, 
+                  maxGSSize = 500, 
+                  pvalueCutoff = 0.05,
+                  verbose = T,
+                  OrgDb='org.Hs.eg.db',
+                  seed = TRUE) 
 GGO_KvsM <- clusterProfiler::simplify(GGO_KvsM)
 GGO_KvsM_df <- GGO_KvsM[,1:11]
 GGO_KvsM_df <- GGO_KvsM_df[order(GGO_KvsM_df$NES, decreasing = TRUE),]
@@ -211,7 +216,7 @@ pairwise.wilcox.test(x = melt_top$value, g = melt_top$Group, p.adjust.method = "
 
 
 # Figure 7F
-ELISA <- read.csv("https://ki.box.com/shared/static/0s0l67ij5uap9k8of5jwhgmk0th934dr.csv", stringsAsFactors = FALSE, row.names = 1)
+ELISA <- read.csv("ELISA.csv", stringsAsFactors = FALSE, row.names = 1)
 ELISA$Group <- factor(ELISA$Group, levels = c("Healthy", "CoV2+", "MIS-C", "Kawasaki"))
 ggplot(ELISA, aes(x=Group, y=log(pg.mL))) + theme_minimal() +
   geom_boxplot(aes(fill=Group)) +
@@ -221,22 +226,62 @@ ggplot(ELISA, aes(x=Group, y=log(pg.mL))) + theme_minimal() +
 pairwise.wilcox.test(ELISA$pg.mL, ELISA$Group, p.adjust.method = "fdr")
 
 
+# Figure 7G
+serine <- GGO_CvsM_df$core_enrichment[GGO_CvsM_df$ID == "GO:0018105"]
+serine <- unlist(str_split(serine, "/"))
+serine_dupl <- CoV_MISC_1$Row.names[CoV_MISC_1$ensembl_ID %in% serine]
+autoAbs_top <- autoAbs_sampleinfo[,c("Identification", "Subjects", "Group", serine_dupl)]
+autoAbs_top <- autoAbs_top[autoAbs_top$Group %in% c("MIS-C", "Healthy", "CoV2+", "Kawasaki"),] 
+melt_top <- reshape2::melt(autoAbs_top, id.vars = c("Identification", "Subjects", "Group"))
+melt_top$Group <- factor(melt_top$Group, levels = c("Healthy", "CoV2+", "MIS-C", "Kawasaki"))
+melt_top <- merge(melt_top, targetinfo, by.x="variable", by.y="duplicates", sort=FALSE)
+melt_top <- melt_top[melt_top$symbols %in% c("MAP2K2", "CSNK1A1", "CSNK2A1", "CSNK1E"),]
+ggplot(melt_top, aes(y=value, x=Group)) + 
+  geom_violin(aes(fill=Group, colour=Group)) +
+  geom_jitter(shape=16, position=position_jitter(0.2), size=0.8, colour="black") +
+  theme_minimal()  + theme(legend.title = element_blank()) +
+  scale_fill_manual(values=c("#2F8AC4", "#E48725", "#A5AA99", "#CD3A8E")) +
+  scale_color_manual(values=c("#2F8AC4", "#E48725", "#A5AA99", "#CD3A8E")) +
+  facet_wrap(~ symbols, ncol = 1, scales = "free") + labs(x=NULL, y="Normalized signal") +
+  ggtitle("GO:0018105 Peptidyl-serine phosphorylation")
+
+# Figure 7H
+# Volcano plot Kawasaki vs MISC
+# Kawasaki vs CoV2
+Kawasaki_MISC <- results(dds1, name="Group_Kawasaki_vs_MIS_C") 
+Kawasaki_MISC_1 <- as.data.frame(Kawasaki_MISC)
+Kawasaki_MISC_1 <- merge(Kawasaki_MISC_1, targetinfo, by.x="row.names", by.y="duplicates", sort=FALSE)
+Kawasaki_MISC_1$Sig <- ifelse(Kawasaki_MISC_1$padj < 0.05, Kawasaki_MISC_1$Sig <- "pAdj < 0.05", Kawasaki_MISC_1$Sig <- "ns")
+library(ggrepel)
+ggplot(subset(Kawasaki_MISC_1, !is.na(Kawasaki_MISC_1$padj)), aes(x = log2FoldChange, y = -log10(pvalue))) +
+  geom_point(aes(color = Sig)) +
+  scale_color_manual(values = c("grey", "#CD3A8E")) +
+  theme_bw(base_size = 12) + theme(legend.position = "bottom") +
+  labs(y="-Log10(pValue)", x="Log2 (Kawasaki/MIS-C)") +
+  xlim(c(-10,15)) + ylim(c(0,20)) +
+  geom_text_repel(
+    data = subset(Kawasaki_MISC_1, padj < 0.001), 
+    aes(label = symbols),
+    size = 2,
+    segment.size = 0.05,
+    segment.colour = "transparent") 
+
+
 sessionInfo()
 #R version 3.6.2 (2019-12-12)
 #Platform: x86_64-apple-darwin15.6.0 (64-bit)
 #Running under: macOS Catalina 10.15.6
 
 #attached base packages:
-#[1] grid      parallel  stats4    stats     graphics  grDevices utils     datasets  methods   base     
+#[1] grid      parallel  stats4    stats     graphics  grDevices utils     datasets  methods   base   
 
 #other attached packages:
-#[1] ComplexHeatmap_2.0.0        UpSetR_1.4.0                VennDiagram_1.6.20          futile.logger_1.4.3        
-#[5] org.Hs.eg.db_3.8.2          AnnotationDbi_1.46.1        clusterProfiler_3.12.0      tibble_3.0.2               
+#[1] ggrepel_0.8.2               ComplexHeatmap_2.0.0        VennDiagram_1.6.20          futile.logger_1.4.3        
+#[5] org.Hs.eg.db_3.8.2          AnnotationDbi_1.46.1        clusterProfiler_3.12.0      tibble_3.0.3               
 #[9] DESeq2_1.24.0               SummarizedExperiment_1.14.1 DelayedArray_0.10.0         BiocParallel_1.18.1        
 #[13] matrixStats_0.56.0          Biobase_2.44.0              GenomicRanges_1.36.1        GenomeInfoDb_1.20.0        
 #[17] IRanges_2.18.3              S4Vectors_0.22.1            BiocGenerics_0.30.0         ggplot2_3.3.2              
-#[21] reshape2_1.4.4              stringr_1.4.0        
-
+#[21] reshape2_1.4.4              stringr_1.4.0
 
 
 
